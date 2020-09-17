c94d5300acdd280d14c12264d94c49864aa48739
Nikhil Patel <nikhil8@usf.edu>
2020-09-17 10:40:16 -0700

    Add project files.

---
+++ TechProject2B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechProject2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
